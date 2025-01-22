6d442d74fa2153caf7ae2f54ac90bf443a8efd5e
ali abdolvandd <aliabdolvandaa76@gmail.com>
2025-01-22 08:59:52 +0330

    The header was added, but it needs submenu

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
+import Header from "@/Header/Header";
 import { Box } from "@mui/material";
 
 export default function Home() {
-  return <Box>hello</Box>;
+  return (
+    <>
+      <Header />
+    </>
+  );
 }
