417c93b0b2eec97ae936f5de408b5df771bfc2eb
ali abdolvandd <aliabdolvandaa76@gmail.com>
2025-01-28 20:43:22 +0330

    fixed router

---
+++ src/app/(app)/cart/page.tsx
@@ -0,0 +1,12 @@
+import { Typography } from "@mui/material";
+import React from "react";
+
+const page = () => {
+  return (
+    <Typography variant="h1" sx={{ pt: 15 }}>
+      cart
+    </Typography>
+  );
+};
+
+export default page;
---
+++ src/app/(app)/profile/page.tsx
@@ -0,0 +1,12 @@
+import { Typography } from "@mui/material";
+import React from "react";
+
+function page() {
+  return (
+    <Typography variant="h1" sx={{ pt: 15 }}>
+      profile
+    </Typography>
+  );
+}
+
+export default page;
