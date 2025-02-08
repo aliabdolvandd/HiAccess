6526198d297d8de3f308f222456a7701ff88c6a9
ali abdolvandd <aliabdolvandaa76@gmail.com>
2025-02-08 14:52:26 +0330

    fix : fixed middleware and session

--- src/lib/session.ts
+++ src/lib/session.ts
@@ -5,9 +5,10 @@ import { redirect } from "next/navigation";
 export async function createSession(token: {
   accessToken: string;
   refreshToken: string;
-  role: string;
 }) {
-  const accessExpiresAt = new Date(Date.now() + 7 * 24 * 60 * 60 * 1000);
+  // const accessExpiresAt = new Date(Date.now() + 7 * 24 * 60 * 60 * 1000);
+  // const refreshExpiresAt = new Date(Date.now() + 30 * 24 * 60 * 60 * 1000);
+  const accessExpiresAt = new Date(Date.now() + 10 * 1000);
   const refreshExpiresAt = new Date(Date.now() + 30 * 24 * 60 * 60 * 1000);
   const cookieStore = await cookies();
 
@@ -26,29 +27,19 @@ export async function createSession(token: {
     sameSite: "lax",
     path: "/",
   });
-  cookieStore.set("role", token.role, {
-    httpOnly: true,
-    secure: true,
-    expires: refreshExpiresAt,
-    sameSite: "lax",
-    path: "/",
-  });
 }
 export async function deleteSession() {
   const cookieStore = await cookies();
   cookieStore.delete("accessToken");
   cookieStore.delete("refreshToken");
-  cookieStore.delete("role");
 }
 export async function auth() {
   const cookieStore = await cookies();
   const accessToken = cookieStore.get("accessToken")?.value;
   const refreshToken = cookieStore.get("refreshToken")?.value;
-  const userRole = cookieStore.get("role")?.value;
   return {
     accessToken,
     refreshToken,
-    userRole,
   };
 }
 
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,101 +1,41 @@
-// import { NextRequest, NextResponse } from "next/server";
-// import { cookies } from "next/headers";
-// import { refreshTokenAction } from "./action/auth/refresh-token";
-
-// // 1. Specify protected and public routes
-// const protectedRoutes = "/dashboard";
-// const publicRoutes = "/";
-
-// export default async function middleware(req: NextRequest) {
-//   // 2. Check if the current route is protected or public
-//   const path = req.nextUrl.pathname;
-//   const isProtectedRoute = path.startsWith(protectedRoutes);
-//   const isPublicRoute = path.includes(publicRoutes);
-
-//   // 3. Decrypt the session from the cookie
-//   const accessToken = (await cookies()).get("accessToken")?.value;
-//   const refreshToken = (await cookies()).get("refreshToken")?.value;
-//   const isLogin = accessToken && refreshToken;
-//   const isLogout = !accessToken && !refreshToken;
-//   const needToRefresh = !accessToken && refreshToken;
-//   if (needToRefresh) {
-//     await refreshTokenAction();
-//     return NextResponse.redirect(new URL(req.nextUrl, req.nextUrl));
-//   }
-//   // 4. Redirect to /login if the user is not authenticated
-//   if (isProtectedRoute && isLogout) {
-//     return NextResponse.redirect(new URL("/auth/login", req.nextUrl));
-//   }
-
-//   // 5. Redirect to /dashboard if the user is authenticated
-//   if (isPublicRoute && !isProtectedRoute && isLogin) {
-//     return NextResponse.redirect(new URL("/dashboard", req.nextUrl));
-//   }
-
-//   return NextResponse.next();
-// }
-
-// // Routes Middleware should not run on
-// export const config = {
-//   matcher: ["/((?!api|_next/static|_next/image|.*\\.png$).*)"],
-// };
 import { NextRequest, NextResponse } from "next/server";
 import { cookies } from "next/headers";
 import { refreshTokenAction } from "./action/auth/refresh-token";
 
-const roleRoutes: Record<string, string[]> = {
-  3: ["/dashboard"], // ADMIN
-  2: ["/sellerPanel"], //SELLER
-  1: ["/"], // USER
-};
+// 1. Specify protected and public routes
+const protectedRoutes = "/dashboard";
+const publicRoutes = "/";
 
 export default async function middleware(req: NextRequest) {
+  // 2. Check if the current route is protected or public
   const path = req.nextUrl.pathname;
-  const cookieStore = await cookies();
+  const isProtectedRoute = path.startsWith(protectedRoutes);
+  const isPublicRoute = path.startsWith(publicRoutes);
 
-  const accessToken = cookieStore.get("accessToken")?.value;
-  const refreshToken = cookieStore.get("refreshToken")?.value;
-  const userRole = Number(cookieStore.get("role")?.value);
-
-  const isLogin = Boolean(accessToken && refreshToken);
+  // 3. Decrypt the session from the cookie
+  const accessToken = (await cookies()).get("accessToken")?.value;
+  const refreshToken = (await cookies()).get("refreshToken")?.value;
+  const isLogin = accessToken && refreshToken;
   const isLogout = !accessToken && !refreshToken;
   const needToRefresh = !accessToken && refreshToken;
-
   if (needToRefresh) {
     await refreshTokenAction();
-    return NextResponse.next();
+    return NextResponse.redirect(new URL(req.nextUrl, req.nextUrl));
   }
-  if (
-    isLogout &&
-    (path.startsWith("/dashboard") || path.startsWith("/seller-panel"))
-  ) {
+  // 4. Redirect to /login if the user is not authenticated
+  if (isProtectedRoute && isLogout) {
     return NextResponse.redirect(new URL("/auth/login", req.nextUrl));
   }
 
-  if (isLogin && userRole) {
-    const allowedPaths = roleRoutes[userRole] || [];
-    const isAllowed = allowedPaths.some((allowedPath) =>
-      path.startsWith(allowedPath)
-    );
-
-    if (!isAllowed) {
-      return NextResponse.redirect(
-        new URL(allowedPaths[0] || "/", req.nextUrl)
-      );
-    }
-
-    return NextResponse.next();
+  // 5. Redirect to /dashboard if the user is authenticated
+  if (isPublicRoute && !isProtectedRoute && isLogin) {
+    return NextResponse.redirect(new URL("/dashboard", req.nextUrl));
   }
 
   return NextResponse.next();
 }
 
+// Routes Middleware should not run on
 export const config = {
-  matcher: [
-    "/dashboard",
-    "/dashboard/:path*",
-    "/sellerPanel",
-    "/sellerPanel/:path*",
-    "/auth/:path*",
-  ],
+  matcher: ["/((?!api|_next/static|_next/image|.*\\.png$).*)"],
 };
