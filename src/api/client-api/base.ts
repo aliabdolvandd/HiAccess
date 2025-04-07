cf757bd381412f0241136369d6a97d377b966ae8
aliabdolvandd <aliabdolvandaa76@gmail.com>
2025-04-08 01:54:20 +0330

    Update base.ts

--- src/api/client-api/base.ts
+++ src/api/client-api/base.ts
@@ -1,7 +1,6 @@
 import axios from "axios";
 const Axios = axios.create({
-  baseURL: "https://backend-divar.liara.run
-",
+  baseURL: "https://backend-divar.liara.run",
 });
 
 export default Axios;
